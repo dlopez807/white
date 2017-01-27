3beb751607c098909aa9f0c45affb3c63b75ac92
Daniel Lopez <dlopez807@gmail.com>
2017-01-26 17:05:13 -0800

    change root to index.html which posts to survey route, add route to sample html and lastnames json

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var bodyParser = require('body-parser');
 var app = express();
+var path = require('path');
+app.set('port', (process.env.PORT || 8791));
+
+app.use(bodyParser.json({limit: '50mb'})); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.text({
+	limit: '50mb'
+}));
 
 app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-	var html = fs.readFileSync('whitepages.html', 'utf-8', function() {
-		console.log('success');
-	});
+app.get('/sample', function(req, res) {
+	res.sendFile(path.join(__dirname + '/whitepages.html'));
+});
+
+app.post('/survey', function(req, res) {
+
+	// var html = fs.readFileSync('whitepages.html', 'utf-8', function() {
+	// 	console.log('success');
+	// });
+	var html = req.body.white;
 
 	var $ = cheerio.load(html);
 
@@ -103,17 +121,25 @@ app.get('/', function(req, res) {
 			
 		}
 	});
-	var html = '<pre>';
+	var out = '<a href=\'/\'>Go back</a><br><br>';
+	out += 'Reload a couple times just to make sure<br><br>';
+	out += 'Copy below and paste into Google Sheets<br><br>';
+	out += '(might need to paste in notepad first to fix formatting)';
+	out += '<pre>';
 	for (var street in streets ) {
 	    if (streets.hasOwnProperty(street)) {
-	        html += street + '<br>';
+	        out += street + '<br>';
 	        streets[street].forEach(function(number) {
-	        	html += number + '<br>';
+	        	out += number + '<br>';
 	        });
 	    }
 	}
-	html += '</pre>';
-	res.send(html);
+	out += '</pre>';
+	res.send(out);
+});
+
+app.get('/lastnames', function(req, res) {
+	res.sendFile(path.join(__dirname + '/lastnames.json'));
 });
 
 app.param('name', function(req, res, next, id) {
@@ -165,113 +191,6 @@ app.get('/lastname/:name', function(req, res) {
 	});	
 });
 
-app.get('/address', function(req, res) {
-
-	var options = {
-		url: 'http://www.whitepages.com/neighbors/d68e2955-d4e6-4510-a15b-686aadd4c3c7',
-		method: 'GET',
-		headers: {
-			'User-Agent': 'request'
-		}	
-	};
-
-	request(options, function(error, response, html) {
-
-		// make sure no errors when making request
-		if (!error) {
-
-			// utilize cheerio library on returned html,
-			// which essentially give us jquery functionality
-			var $ = cheerio.load(html);
-
-			// define variables we're going to capture
-			//var title, release, rating;
-			//var brand, title, price;
-
-			
-			//console.log($('body > form').attr('name'));
-			//console.log(response);
-			console.log(html);
-			
-			$('.FL_LabelxSmall').each(function() {
-				var text = $(this).text();
-				console.log(text);
-			})
-
-			// $('.unstyled.grid-list-2.no-overflow > li').each(function() {
-			// 	var address = $('.title', this).text();
-			// 	var text = $('.subtitle', this).text().trim();
-			// 	if (text.indexOf('No current residents listed for this location') < 0) {
-			// 		name = text.split(' res')[0].split(' and')[0];
-
-			// 		console.log(name);
-			// 		console.log(address);
-			// 		console.log('');
-			// 	}
-			// });
-			//res.send('check console');
-			res.send(html);
-		}
-		else
-			console.log('error');
-
-		// fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
-
-		//  	console.log('file successfully written')
-		//  });
-	});
-
-	 
-
-	
-});
-
-app.get('/test', function(req, res) {
-	// request('/lastnames/lopez', function(error, response, body) {
-	// 	if (!error) {
-	// 		console.log(response);
-	// 		console.log(body);
-	// 	}
-	// })
-	console.log('test');
-	res.send('test');
-})
-
-app.get('/foo', function(req, res) {
-	console.log('foo');
-	var options = {
-		url: '/test',
-		method: 'GET'
-	};
-	request(options, function(error, response, body) {
-		if (!error) {
-			console.log(response);
-			console.log(body);
-		}
-		else
-			console.log('error');
-	})
-	res.send('foo');
-})
-var port = 8791;
-app.listen(port);
-console.log('magic happens on port ' + port);
+app.listen(app.get('port'));
+console.log('magic happens on port ' + app.get('port'));
 exports = module.exports = app;
-
-/*
-$('.unstyled.grid-list-2.no-overflow > li .subtitle').each(function() {
- lastnames.push($(this).text()); 
-})
-
-$('.unstyled.grid-list-2.no-overflow > li').each(function() {
-	var address = $('.title', this).text();
-	var text = $('.subtitle', this).text().trim();
-	if (text.indexOf('No current residents listed for this location') < 0) {
-		name = text.split(' res')[0].split(' and')[0];
-
-		console.log(name);
-		console.log(address);
-		console.log('');
-	}
-});
-*/
