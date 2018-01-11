f932bbc17b56518f6ee802b40a1d0cc1b911e5ca
Daniel Lopez <dlopez807@gmail.com>
2018-01-11 12:57:53 -0800

    Enable CORS

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var app = express();
 var path = require('path');
 app.set('port', (process.env.PORT || 8791));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({limit: '50mb'})); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })); // for parsing application/x-www-form-urlencoded
